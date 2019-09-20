f6d44f302e350720d59afc669f69f8fdbce4ba60
Danny Spijker <danny.spijker@flinders.nl>
2019-09-20 11:20:57 +0200

    Added a edit action to projects

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -8,7 +8,7 @@
                 <a href="/projects" class="text-gray text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
             </p>
 
-            <a href="/projects/create" class="button">Create New Project</a>
+            <a href="{{ $project->path() . '/edit' }}" class="button">Edit Project</a>
         </div>
     </header>
 
